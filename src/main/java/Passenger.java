446979caeadf3facbfbb80b7d8959af82d208319
essamcreates <mrm.hussain@hotmail.co.uk>
2023-07-25 11:48:27 +0100

    completed passenger test

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,41 @@
 public class Passenger {
+
+//    properties
+    private String name;
+    private String teleNumber;
+    private int id;
+
+//    constructor
+
+    public Passenger(String name, String teleNumber, int id){
+
+        this.name = name;
+        this.teleNumber = teleNumber;
+        this.id = id;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeleNumber() {
+        return teleNumber;
+    }
+
+    public void setTeleNumber(String teleNumber) {
+        this.teleNumber = teleNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,4 +1,58 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PassengerTest {
-    
+
+    private Passenger passenger;
+
+    @BeforeEach
+
+   public void setUp(){
+
+        passenger = new Passenger("Tom", "449876", 10);
+
+    }
+
+
+    @Test
+
+    public void canGetName(){
+        assertThat(passenger.getName()).isEqualTo("Tom");
+    }
+
+    @Test
+
+    public void canSetName(){
+        passenger.setName("Harry");
+        assertThat(passenger.getName()).isEqualTo("Harry");
+    }
+
+    @Test
+
+    public void canGetTeleNumber(){
+        assertThat(passenger.getTeleNumber()).isEqualTo("449876");
+    }
+
+    @Test
+
+    public void canSetTeleNumber(){
+        passenger.setTeleNumber("567");
+        assertThat(passenger.getTeleNumber()).isEqualTo("567");
+    }
+
+    @Test
+
+    public void canGetId(){
+        assertThat(passenger.getId()).isEqualTo(10);
+    }
+
+    @Test
+
+    public void canSetId(){
+        passenger.setId(12);
+        assertThat(passenger.getId()).isEqualTo(12);
+    }
 
 }
