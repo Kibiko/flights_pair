7e7c2e9d18a419a3a5cfa38dc30a40b4ed6a14ae
Kibiko <kevinjunchan@gmail.com>
2023-07-25 12:09:00 +0100

    flights and flighttest completed

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    //properties
+
+    private String destination;
+    private int id;
+    private ArrayList<Passenger> passengers;
+
+    //constructor
+
+    public Flight(String destination, int id){
+        this.destination = destination;
+        this.id = id;
+        this.passengers = new ArrayList<>();
+    }
+
+    //methods
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public int getSize(){
+        return passengers.size();
+    }
+
+    public void addPassenger(Passenger passenger){
+        this.passengers.add(passenger);
+    }
+
+    public void removePassenger(Passenger passenger){
+        this.passengers.remove(passenger);
+    }
+
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,58 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FlightTest {
+
+    private Flight flight;
+
+    @BeforeEach
+    public void setUp(){
+        flight = new Flight("Copenhagen",12341);
+    }
+
+    @Test
+    public void canGetDestination(){
+        assertThat(flight.getDestination()).isEqualTo("Copenhagen");
+    }
+
+    @Test
+    public void canSetDestination(){
+        flight.setDestination("Milan");
+        assertThat(flight.getDestination()).isEqualTo("Milan");
+    }
+
+    @Test
+    public void canGetId(){
+        assertThat(flight.getId()).isEqualTo(12341);
+    }
+
+    @Test
+    public void canSetId(){
+        flight.setId(13654);
+        assertThat(flight.getId()).isEqualTo(13654);
+    }
+
+    @Test
+    public void canGetSize(){
+        assertThat(flight.getSize()).isEqualTo(0);
+    }
+
+    @Test
+    public void canAddPassenger(){
+        Passenger passenger = new Passenger("Kev", "134256357",123);
+        flight.addPassenger(passenger);
+        assertThat(flight.getSize()).isEqualTo(1);
+    }
+
+    @Test
+    public void canRemovePassenger(){
+        Passenger passenger = new Passenger("Kev", "134256357",123);
+        flight.addPassenger(passenger);
+        flight.removePassenger(passenger);
+        assertThat(flight.getSize()).isEqualTo(0);
+    }
+
+
 }
