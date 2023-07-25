e8825d266d8cd8d0036097034dcd24fbf95048e9
essamcreates <mrm.hussain@hotmail.co.uk>
2023-07-25 12:45:58 +0100

    completed airport class and test

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+
 public class Airport {
+
+    private ArrayList<Flight> availableFlights;
+    private ArrayList<Passenger> userbase;
+
+    public Airport(){
+        this.availableFlights = new ArrayList<>();
+        this.userbase = new ArrayList<>();
+    }
+
+    public ArrayList<Flight> getAvailableFlights() {
+        return availableFlights;
+    }
+
+    public ArrayList<Passenger> getUserbase() {
+        return userbase;
+    }
+
+    public int getFlightSize(){
+        return this.availableFlights.size();
+    }
+
+    public int getUserSize(){
+        return this.userbase.size();
+    }
+
+    public void addNewFlight(Flight flight){
+        this.availableFlights.add(flight);
+    }
+
+    public void cancelFlight(Flight flight){
+        this.availableFlights.remove(flight);
+    }
+
+    public void addUser(Passenger user){
+        this.userbase.add(user);
+    }
+
+    public void removeUser(Passenger user){
+        this.userbase.remove(user);
+    }
+
+    public void bookUser(Passenger user, Flight flight){
+        this.removeUser(user);
+        flight.addPassenger(user);
+    }
+
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,2 +1,71 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class AirportTest {
+
+    private Airport airport;
+
+    private Flight flight;
+    private Passenger user;
+
+    @BeforeEach
+
+    public void setUp(){
+
+        airport = new Airport();
+        flight = new Flight("Madrid", 456);
+        user = new Passenger("Tom", "999", 5);
+        airport.addUser(user);
+        airport.addNewFlight(flight);
+
+    }
+
+    @Test
+    public void canGetAvailableFlights(){
+        assertThat(airport.getAvailableFlights().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void canGetUserbase(){
+        assertThat(airport.getUserbase().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void canAddNewFlight(){
+        Flight flight2 = new Flight("Paris", 457);
+        airport.addNewFlight(flight2);
+        assertThat(airport.getFlightSize()).isEqualTo(2);
+    }
+
+    @Test
+    public void canCancelFlight(){
+        airport.cancelFlight(flight);
+        assertThat(airport.getFlightSize()).isEqualTo(0);
+    }
+
+    @Test
+    public void canAddUser(){
+        Passenger user2 = new Passenger("Paris", "57", 80);
+        airport.addUser(user2);
+        assertThat(airport.getUserSize()).isEqualTo(2);
+    }
+
+    @Test
+    public void canRemoveUser(){
+        airport.removeUser(user);
+        assertThat(airport.getUserSize()).isEqualTo(0);
+    }
+
+    @Test
+    public void canBookUser(){
+
+        airport.bookUser(user, flight);
+        assertThat(airport.getUserSize()).isEqualTo(0);
+        assertThat(flight.getSize()).isEqualTo(1);
+
+    }
+
+
 }
